fecebe69692e75b78d6b0cab5b70819ffce50647
Tomasz Rohda <tomaszrohda@wp.pl>
2022-04-27 21:08:48 +0200

    Add Home and UserDashboard components. Create basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import UserDashboard from "./components/UserDashboard";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/userdashboard" element={<UserDashboard />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
