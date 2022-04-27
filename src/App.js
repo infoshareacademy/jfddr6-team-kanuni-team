2247b70d2ed9980b522ff7b24d2615aca763c30d
Tomasz Rohda <tomaszrohda@wp.pl>
2022-04-27 14:57:22 +0200

    create Footer and Navbar componenets files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
