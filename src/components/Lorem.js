5827eb01bd8bfddf0fcd0debf9dcf5287bb4e91b
Weronika Kołodziej <weronikakolodziej@op.pl>
2022-04-29 10:32:38 +0200

    added new components Offer, Header,Lorem

---
+++ src/components/Lorem.js
@@ -0,0 +1,11 @@
+const Text= () => {
+    
+    return (
+
+    <p>Duis sapien urna, rhoncus non metus vitae, iaculis molestie nisi. Aenean quis accumsan diam. Aenean gravida augue libero, a tincidunt libero porta nec. In varius ligula sit amet arcu aliquam porta. Donec at felis sollicitudin, volutpat turpis vitae, eleifend enim. Aliquam dignissim orci ac sapien ultricies blandit. Sed pretium, tellus vitae ullamcorper condimentum,
+    justo risus hendrerit odio, in congue justo metus dictum diam. Mauris sed turpis ut mauris pulvinar dignissim a rhoncus turpis. Quisque ut orci non odio consequat tincidunt ut in est. Fusce diam sapien, egestas vel dolor quis, pellentesque finibus turpis. Maecenas molestie nisi quis tempor blandit. Morbi finibus tristique viverra. Donec imperdiet lectus nunc, vitae dapibus dui placerat quis. Proin ultrices, purus ac pellentesque lacinia, velit purus fringilla orci, id fringilla lorem est ac elit.
+    </p>
+    )
+}
+
+export default Text; 
---
+++ src/components/Offer.js
@@ -0,0 +1,24 @@
+import Text from "./Lorem"
+import Button from "./Button";
+import "./Offer.css"
+const Offer = () => {
+
+
+        
+    return(
+
+        <div className="contentOffer">
+                <div className="aboutUs">
+                        <Text />
+                </div>
+
+                <button className="buttonOffer" onClick={Button}>Umów się na wizytę</button>
+
+           
+           
+        </div>
+
+    )
+}
+
+export default Offer;
