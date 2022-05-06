74f20d5f44066a4500c8e14a945b4a337ed58952
Weronika Kołodziej <weronikakolodziej@op.pl>
2022-05-06 09:07:25 +0200

    added Frist page style

---
+++ src/components/Auxiliary/Img.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Img = (props) => (
+  <div>
+    <img className={props.className} src={props.src} />
+  </div>
+);
+
+export default Img;
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,18 +2,24 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <form>
-      <h2 className="head">Kontakt</h2>
-      <input className="mailArea" type="text" placeholder="twójemail@op.pl" />
+    <div className="contactContenet">
+      <form>
+        <h2 className="head">Kontakt</h2>
+        <input className="mailArea" type="text" placeholder="twójemail@op.pl" />
 
-      <textarea
-        rows="10"
-        cols="50"
-        className="questionArea"
-        placeholder="Masz pytanie? Napisz do nas"
-      />
-      <button className="sendBtn">Wyślij</button>
-    </form>
+        <textarea
+          rows="10"
+          cols="50"
+          className="questionArea"
+          placeholder="Masz pytanie? Napisz do nas"
+        />
+        <button className="sendBtn">Wyślij</button>
+      </form>
+      <div className="callUs">
+        <p>Lub zadzoń do nas!</p>
+        <p> +72 828 828 828</p>
+      </div>
+    </div>
   );
 };
 
