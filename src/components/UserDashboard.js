9fee58bd59289a3dfc172609f070669036d979f8
Jakub Biernacik <kubabier@wp.pl>
2022-05-06 11:44:32 +0200

    add skeleton for user dashboard

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,5 +1,25 @@
+import Header from "./Header/Header";
+
 const UserDashboard = () => {
-  return <div>User Dashboard</div>;
+  return (
+    <>
+      <Header />
+      <div>
+        <div>
+          <h3>Twoje zaplanowane wizyty:</h3>
+          <div> wizyta/brak wizyt <button>Anuluj</button> </div>
+        </div>
+        <button>Umów Wizytę</button>
+        <div>
+          <h3>Twoje zrealizowane wizyty:</h3>
+          <div>00/00/00 NazwaPakietu</div>
+        </div>
+
+      </div>
+    </>
+
+  );
+
 };
 
 export default UserDashboard;
