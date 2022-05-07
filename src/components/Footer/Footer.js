5321c6b3fe12737e44268ee3f6519aa305d30f79
skjoeldkrona <skjoeldkrona@gmail.com>
2022-05-07 09:31:58 +0200

    feat: footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,15 @@
+import "./Footer.css"
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+  <div className="footer">
+    <ul>
+      <li>E-mail: czystsze@niebedzie.pl</li>
+      <li>Telefon: 666 777 888</li>
+      <li>Adres: ul. Czysta 11, 17-256 Czyścioszkowo</li>
+    </ul>
+  </div>)
+  ;
 };
 
 export default Footer;
