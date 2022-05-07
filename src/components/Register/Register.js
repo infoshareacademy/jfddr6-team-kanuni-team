80efec3f6260c4e6740584d2921c762df62ecaad
Tomasz Rohda <tomaszrohda@wp.pl>
2022-05-07 11:38:10 +0200

    Add "password exist" veryfication

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,14 +6,29 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
 import { auth, db } from '../../data/db';
 
-const registerUser = async (email, password) => {
+const registerUserAuth = async (email, password) => {
   // todo nazwa auth user
-  const response = await createUserWithEmailAndPassword(auth, email, password);
-  // console.log(response.user.uid);
+  const response = await createUserWithEmailAndPassword(auth, email, password).catch((error) => {
+    switch (error.code) {
+      case 'auth/email-already-in-use':
+        alert(`email ${email} is already in use`);
+        break;
+      case 'auth/invalid-email':
+        alert(`Email address ${email} is invalid.`);
+        break;
+      case 'auth/operation-not-allowed':
+        alert(`Error during sign up.`);
+        break;
+      default:
+        console.log(error.message);
+        break;
+    }
+  });
+  console.log(response);
   return response.user;
 };
 
-const createUser = async ({ id, name, lastName }) => {
+const createUserMetaDate = async ({ id, name, lastName }) => {
   await setDoc(doc(db, 'users', id), {
     name,
     lastName,
@@ -28,14 +43,13 @@ const Register = () => {
 
   const handlerRegister = async (e) => {
     e.preventDefault();
-    const registeredUser = await registerUser(emailInput, passwordInput);
+    const registeredUser = await registerUserAuth(emailInput, passwordInput);
     const userId = registeredUser.uid;
-    console.log({ userId });
-    await createUser({ id: userId, name: nameInput, lastName: lastNameInput }); //todo zmienić nazwę
-    // setNameInput('');
-    // setLastNameInput('');
-    // setEmailInput('');
-    // setPasswordInput('');
+    await createUserMetaDate({ id: userId, name: nameInput, lastName: lastNameInput }); //todo zmienić nazwę
+    setNameInput('');
+    setLastNameInput('');
+    setEmailInput('');
+    setPasswordInput('');
   };
 
   return (
