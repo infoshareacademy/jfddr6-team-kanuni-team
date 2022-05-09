16b42b78f82cd6f0f5df9792a8e72d5adfd23243
Weronika Kołodziej <weronikakolodziej@op.pl>
2022-05-09 11:06:33 +0200

    fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import Logo from '../NavbarElements/Logo.js';
 import NavbarLinks from '../NavbarElements/NavbarLinks.js';
-import "./Navbar.css"
+import './Navbar.css';
 
 const Navbar = () => {
   return (
