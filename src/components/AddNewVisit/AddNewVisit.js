9c64d83fd720f1d701eca7211cf8a4dc209b6344
Tomasz Rohda <tomaszrohda@wp.pl>
2022-05-10 18:37:42 +0200

    add logic in AddNewVisit.js

--- src/components/AddNewVisit/AddNewVisit.js
+++ src/components/AddNewVisit/AddNewVisit.js
@@ -1,41 +1,58 @@
 import { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
+import Button from '../Auxiliary/Button';
 import 'react-day-picker/dist/style.css';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { arrayUnion, updateDoc, doc } from 'firebase/firestore';
 import { db } from '../../data/db';
 
-const AddNewVisit = () => {
+const AddNewVisit = (userUid) => {
   const [selectedDay, setSelectedDay] = useState('');
+  const [timeInput, setTimeInput] = useState('');
+  const [packageInput, setPackageInput] = useState('');
+  const [summary, setSummary] = useState(false);
+
+  const handlerClick = (day) => {
+    setSelectedDay(day);
+    console.log(day.toLocaleDateString());
+  };
+
+  const summaryHandler = (e) => {
+    e.preventDefault();
+    setSummary(true);
+    // console.log(timeInput, packageInput, selectedDay);
+  };
+
+  const sendVisitToFirebase = async () => {
+    const timeStampDateSeconds = new Date(selectedDay).getTime();
+    const timeStampDatePlusHour = () => {
+      if (timeInput === '8:00') {
+        console.log(timeStampDateSeconds + 28800000);
+        return timeStampDateSeconds + 28800000;
+      } else if (timeInput === '11:00') {
+        return timeStampDateSeconds + 39600000;
+      } else if (timeInput === '14:00') {
+        return timeStampDateSeconds + 50400000;
+      }
+    };
+
+    await updateDoc(doc(db, 'users', userUid.userUid), {
+      visits: arrayUnion({
+        date: new Date(timeStampDatePlusHour()),
+        package: packageInput,
+        isDone: false,
+      }),
+    });
+    setSelectedDay('');
+    setTimeInput('');
+    setPackageInput('');
+    setSummary(false);
+  };
 
   const disabledDays = [{ from: new Date(2022, 1, 1), to: new Date() }];
 
-  const handlerClick = (day) => {
-    setSelectedDay(day.toDateString());
-    console.log(selectedDay);
-  };
-
-  const getVisitByDay = (day) => {
-    const visitsRef = collection(db, 'visits');
-    const visitQuery = query(visitsRef, where('day', '==', day));
-
-    // const visits = await getDocs(visitQuery);
-
-    // visits.forEach((doc) => {
-    //   console.log(doc.id, ' => ', doc.data());
-    // });
-
-    return getDocs(visitQuery).then((visit) => {
-      console.log({ visit });
-      const visitFromDb = visit.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      return visitFromDb;
-    });
-  };
   return (
     <>
-      <h1>Umów wizytę</h1>
+      <h1>Umów się na czyszczonko</h1>
 
       <DayPicker
         mode="single"
@@ -46,18 +63,58 @@ const AddNewVisit = () => {
         onSelect={handlerClick}
         captionLayout="dropdown"
       />
-      <form>
-        {selectedDay && (
-          <>
-            <p>{`Selected day ${selectedDay}`}</p>
-            <p>Select Time:</p>
-            {getVisitByDay(selectedDay).then((visit) => {
-              console.log({ visit });
-            })}
-            <select name="time"></select>
-          </>
-        )}
-      </form>
+
+      {selectedDay && (
+        <>
+          <form>
+            <label htmlFor="time">Wybierz godzinę</label>
+            <select
+              name="time"
+              id="time"
+              onChange={(e) => setTimeInput(e.target.value)}
+              defaultValue=""
+            >
+              <option value="" disabled>
+                Wybierz godzinę
+              </option>
+              <option value="8:00">8:00</option>
+              <option value="11:00">11:00</option>
+              <option value="14:00">14:00</option>
+            </select>
+
+            <label htmlFor="package">Wybierz pakiet</label>
+            <select
+              name="package"
+              id="package"
+              onChange={(e) => setPackageInput(e.target.value)}
+              defaultValue=""
+            >
+              <option value="" disabled>
+                Wybierz pakiet
+              </option>
+              <option value="Pakiet Platinum">Pakiet Platinum</option>
+              <option value="Pakiet Gold">Pakiet Gold</option>
+              <option value="Pakiet Silver">Pakiet Silver</option>
+              <option value="Pakiet Bronze">Pakiet Bronze</option>
+              <option value="Pakiet Wood">Pakiet Wood</option>
+              <option value="Pakiet Plastic">Pakiet Plastic</option>
+            </select>
+
+            {timeInput && packageInput ? (
+              <Button buttonText={'Podsumowanie'} onClick={summaryHandler} />
+            ) : null}
+          </form>
+          {summary && (
+            <div>
+              <h2>Podsumowanie umówienia wizyty:</h2>
+              <p>{`Dzień: ${selectedDay.toLocaleDateString()}`}</p>
+              <p>{`Godzina: ${timeInput}`}</p>
+              <p>{`Pakiet: ${packageInput}`}</p>
+              <Button buttonText={'Wyślij'} onClick={sendVisitToFirebase} />
+            </div>
+          )}
+        </>
+      )}
     </>
   );
 };
