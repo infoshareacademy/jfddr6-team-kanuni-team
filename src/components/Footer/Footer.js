92b6a5a0db94000ad3194060411fe750279f51b4
Weronika Kołodziej <weronikakolodziej@op.pl>
2022-05-11 14:50:54 +0200

    style contact

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,25 @@
-import "./Footer.css"
-import "./GitHubLogo.png"
+import './Footer.css';
+
 const Footer = () => {
   return (
     <div className="footer">
       © Kanuni Team 2022
       <ul>
-        <li><a href="https://github.com/skjoeldkrona"></a>skjoeldkrona</li>
-        <li><a href="https://github.com/wejjafly"></a>wejjafly</li>
-        <li><a href="https://github.com/trohda"></a>trohda</li>
-        <li><a href="https://github.com/jakub-biernacik"></a>jakub-biernacik</li>
+        <li>
+          <a href="https://github.com/skjoeldkrona"></a>skjoeldkrona
+        </li>
+        <li>
+          <a href="https://github.com/wejjafly"></a>wejjafly
+        </li>
+        <li>
+          <a href="https://github.com/trohda"></a>trohda
+        </li>
+        <li>
+          <a href="https://github.com/jakub-biernacik"></a>jakub-biernacik
+        </li>
       </ul>
-    </div>)
-    ;
+    </div>
+  );
 };
 
 export default Footer;
