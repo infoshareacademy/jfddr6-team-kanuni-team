b6d4370db8de3a48f69ac1e7adc3f854c2140096
Tomasz Rohda <tomaszrohda@wp.pl>
2022-05-12 12:18:48 +0200

    add names to contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,41 +3,43 @@ import './Contact.css';
 const Contact = () => {
   return (
     <div className="contact" id="contact">
-      <section className="contact_wrapper">
-        <div className="contact_info">
-          <h3 className="titleleft">kontakt</h3>
+      <form netlify>
+        <section className="contact_wrapper">
+          <div className="contact_info">
+            <h3 className="titleleft">kontakt</h3>
 
-          <div className="callUs">
-            <p>Lub zadzoń do nas!</p>
-            <p> +72 828 828 828</p>
-          </div>
-        </div>
-
-        <div class="contact_msg">
-          <h3 class="title">napisz do nas</h3>
-
-          <div class="form_fild">
-            <div class="input_group">
-              <input type="text" class="input" required />
-              <label class="placeholder">twoje imie</label>
-            </div>
-
-            <div class="input_group">
-              <input type="text" class="input" required />
-              <label class="placeholder">twój adres email</label>
-            </div>
-
-            <div class="input_group">
-              <textarea class="input input_textarea " rows="6" required></textarea>
-              <label class="placeholder textarea">napisz do nas wiadomość</label>
-            </div>
-
-            <div class="input_group">
-              <input type="submit" class="btn" value="Wyślij" />
+            <div className="callUs">
+              <p>Lub zadzoń do nas!</p>
+              <p> +72 828 828 828</p>
             </div>
           </div>
-        </div>
-      </section>
+
+          <div class="contact_msg">
+            <h3 class="title">napisz do nas</h3>
+
+            <div class="form_fild">
+              <div class="input_group">
+                <input name="name" type="text" class="input" required />
+                <label class="placeholder">twoje imie</label>
+              </div>
+
+              <div class="input_group">
+                <input name="email" type="text" class="input" required />
+                <label class="placeholder">twój adres email</label>
+              </div>
+
+              <div class="input_group">
+                <textarea name="message" class="input input_textarea " rows="6" required></textarea>
+                <label class="placeholder textarea">napisz do nas wiadomość</label>
+              </div>
+
+              <div class="input_group">
+                <input type="submit" class="btn" value="Wyślij" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </form>
     </div>
   );
 };
